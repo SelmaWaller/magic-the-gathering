e0e77de9a74c9262459905dea23d2d718c03ea39
Selma Waller <selmawaller@gmail.com>
2020-03-24 12:34:58 +0100

    new card-specific path: card

--- src/components/cards.js
+++ src/components/cards.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import {Link} from 'react-router-dom';
 
-const Cards = ({id, name, image, set, collectorNumber}) => {
+const Cards = ({name, image, set, collectorNumber}) => {
   return (
     <div className="innerCard linkedCard boxShadow cards">
-      <Link to={`/card-specific/${set}/${collectorNumber}`}>
+      <Link to={`/card/${set}/${collectorNumber}`}>
         <img src={image} alt={name} title={name} />
       </Link>
     </div>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import App from './App';
 import Overview from './pages/Overview';
 import CardSpecific from './pages/CardSpecific';
+import About from './pages/About';
 import Contact from './pages/Contact';
 
 import {Provider} from 'react-redux';
@@ -27,10 +28,11 @@ ReactDOM.render(
         <Switch>
           <Route path="/" exact component={Overview} />
           <Route
-            path="/card-specific/:set/:collectorNumber"
+            path="/card/:set/:collectorNumber"
             exact
             component={CardSpecific}
           />
+          <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
         </Switch>
       </App>
